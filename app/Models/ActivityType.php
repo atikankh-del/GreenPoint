64b2614db1ebef2966bf3aef08c65de11064fa80
pornnutcha27 <kanokporn.je@rmuti.ac.th>
2026-09-17 18:58:49 +0700

    Add stable tree activity classification

--- app/Models/ActivityType.php
+++ app/Models/ActivityType.php
@@ -1,4 +1,4 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
-class ActivityType extends Model { protected $guarded=[]; public function posts(){return $this->hasMany(Post::class);} }
+class ActivityType extends Model { protected $guarded=[]; protected $casts=['active'=>'boolean','counts_trees'=>'boolean']; public function posts(){return $this->hasMany(Post::class);} }
